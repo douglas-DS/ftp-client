21bc6830fca0479847fa84b6985dc2869b9edf1d
Douglas Souza <douglas_souza03@hotmail.com>
2019-03-26 15:02:22 -0300

    spring integration annotations

--- src/main/java/br/com/douglas/ftpclient/FtpClientApplication.java
+++ src/main/java/br/com/douglas/ftpclient/FtpClientApplication.java
@@ -2,8 +2,12 @@ package br.com.douglas.ftpclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
 
 @SpringBootApplication
+@EnableIntegration
+@IntegrationComponentScan
 public class FtpClientApplication {
 
     public static void main(String[] args) {
